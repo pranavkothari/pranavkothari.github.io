05144fba2134e8cde640cd48c5668abcad8d8ab5
pranavkothari28 <60580591+pranavkothari28@users.noreply.github.com>
2020-02-12 22:51:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 // Hi there this is js file
+$(document).scroll(function (){
+    var y = $(this).scrollTop();
+    if (y > 300) {
+        $(".footer-top").fadeIn();
+    }
+    else {
+        $(".footer-top").fadeOut();
+    }
+});
+
+$(document).ready(function(){
+    $('.footer-top').click(function(){
+        scrollTo(0,0);
+    });
+});
